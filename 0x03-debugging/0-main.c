b99aa2e0adca227ea1286fc7093984e37ba0a17e
Valkth <baagaala800@gmail.com>
2023-02-27 11:22:22 -0800

    0-reset_to_98.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * main - Test function for positive or negative
- * Return: 0
+ * main - tests the positive_or_negative function with a case of 0
+ *
+ * Return: Always 0
  */
 
 int main(void)
 {
- int i;
+        int zero = 0;
 
- i = 0;
- positive_or_negative(i);
+        positive_or_negative(zero);
 
-return (0);
+        return (0);
 }
